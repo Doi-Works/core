2774bff628811f607abdd5c2d399dc04250bc3aa
Nico Krause <nico@le-space.de>
2018-05-03 19:44:56 +0200

    changed address prefix from nc to dc, tn to td and ncrt to dcrt, adding logging

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -235,7 +235,7 @@ hashMerkleRoot: 234651063df5f8b01ecc2fc3a134fa1cb9dc9da9cce0149049483ba1b1469dfb
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4};
 
-        bech32_hrp = "nc";
+        bech32_hrp = "dc";
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
 
@@ -440,7 +440,7 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x35, 0x87, 0xCF};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94};
 
-        bech32_hrp = "tn";
+        bech32_hrp = "td";
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_test, pnSeed6_test + ARRAYLEN(pnSeed6_test));
 
@@ -592,7 +592,7 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x35, 0x87, 0xCF};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94};
 
-        bech32_hrp = "ncrt";
+        bech32_hrp = "dcrt";
 
         assert(mapHistoricBugs.empty());
     }
--- src/names/main.cpp
+++ src/names/main.cpp
@@ -616,7 +616,8 @@ CheckNameTransaction (const CTransaction& tx, unsigned nHeight,
 									 " on an unexpired name"));
     }
 
-  	//TODO any checkou on OP_NAME_DOI
+  	//TODO any check on OP_NAME_DOI
+  	LogPrintf ("CheckNameTransaction Step5 (please add some checks for OP_NAME_DOI here!\n");
 
   /* We don't have to specifically check that miners don't create blocks with
      conflicting NAME_FIRSTUPDATE's, since the mining's CCoinsViewCache
